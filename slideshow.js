d000e03e38e935a6700d3f60a3e9efe3cc8808c9
Max <max@maximilian.co.nz>
2022-08-06 23:18:29 +1200

    Added custom HTML elements😎

--- slideshow.js
+++ slideshow.js
@@ -1,2 +1,31 @@
 console.log("Slideshow");
 
+// Make the custom HTML elements
+class SlideshowElement extends HTMLElement {
+    constructor() {
+        super();
+
+
+        // Add the buttons and image
+        this.innerHTML = `
+        <img src="" alt="">
+        <button id="slideshowLeft">⬅️</button>
+        <button id="slideshowRight">➡️</button>
+        `;
+    }
+}
+
+
+// Make the custom HTML elements
+class SlideshowDataElement extends HTMLElement {
+    constructor() {
+        super();
+
+        // Hide all of the images
+        this.style.display = "none";
+    }
+}
+
+// Register the custom HTML
+customElements.define("slide-show", SlideshowElement);
+customElements.define("slide-show-data", SlideshowDataElement);
